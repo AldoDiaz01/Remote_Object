4ba1743bea9cf63c3174eacc347f69e0beb97cc6
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-03-01 13:33:29 -0600

    fix ecuacionlineal

--- src/E1/ObjetoRemoto.java
+++ src/E1/ObjetoRemoto.java
@@ -2,6 +2,7 @@ package E1;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.text.DecimalFormat;
 
 public class ObjetoRemoto extends UnicastRemoteObject implements InterfaceRemota {
     public ObjetoRemoto() throws RemoteException {
@@ -28,7 +29,6 @@ public class ObjetoRemoto extends UnicastRemoteObject implements InterfaceRemota
 
         if (resto <= 0) {
             System.out.println("No se pueden ingresar datos menores a 0");
-            System.out.println("Todo meco este vato si o no raza? xdxdxdxdxdxdx");
             return new double[]{};
         } else {
             double raiz = Math.sqrt(resto);
@@ -36,13 +36,21 @@ public class ObjetoRemoto extends UnicastRemoteObject implements InterfaceRemota
             double preResPos = (-b) + raiz;
             double preResNeg = (-b) - raiz;
 
-            double resPos = preResPos / (2 * a);
-            double resNeg = preResNeg / (2 * a);
+            double x1 = preResPos / (2 * a);
+            double x2 = preResNeg / (2 * a);
 
-            double eqPos = (a * Math.pow(resPos, 2)) + (b * resPos) + c;
-            double eqNeg = (a * Math.pow(resNeg, 2)) + (b * resNeg) + c;
+            DecimalFormat df = new DecimalFormat("#.0000");
 
-            return new double[]{resPos, resNeg, eqPos, eqNeg};
+            x1 = Double.parseDouble(df.format(x1));
+            x2 = Double.parseDouble(df.format(x2));
+
+            double ecX1 = (a * Math.pow(x1, 2)) + (b * x1) + c;
+            double ecX2 = (a * Math.pow(x2, 2)) + (b * x2) + c;
+
+            ecX1 = Double.parseDouble(df.format(ecX1));
+            ecX2 = Double.parseDouble(df.format(ecX2));
+
+            return new double[]{x1, x2, ecX1, ecX2};
         }
     }
 }
